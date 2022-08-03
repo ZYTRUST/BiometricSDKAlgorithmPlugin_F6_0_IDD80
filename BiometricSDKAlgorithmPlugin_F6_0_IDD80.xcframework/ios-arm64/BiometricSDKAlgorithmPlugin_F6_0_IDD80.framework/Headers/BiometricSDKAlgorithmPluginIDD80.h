8e01f6dcac6184590160cacedd15bac93557ec2b
ZYTRUST SA <ztmobile@zytrust.com>
2022-08-03 16:24:55 -0500

    4.32.1

---
+++ BiometricSDKAlgorithmPlugin_F6_0_IDD80.xcframework/ios-arm64/BiometricSDKAlgorithmPlugin_F6_0_IDD80.framework/Headers/BiometricSDKAlgorithmPluginIDD80.h
@@ -0,0 +1,15 @@
+//
+//  BiometricSDKAlgorithmPlugin_F6_0_IDD80.h
+//  BiometricSDKAlgorithmPlugin_F6_0_IDD80
+//
+//  Created by Konrad Szewczyk on 26/02/2020.
+//  Copyright © 2020 IDEMIA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BiometricSDKAlgorithmPlugin_F6_0_IDD80.
+FOUNDATION_EXPORT double BiometricSDKAlgorithmPlugin_F6_0_IDD80VersionNumber;
+
+//! Project version string for BiometricSDKAlgorithmPlugin_F6_0_IDD80.
+FOUNDATION_EXPORT const unsigned char BiometricSDKAlgorithmPlugin_F6_0_IDD80VersionString[];
