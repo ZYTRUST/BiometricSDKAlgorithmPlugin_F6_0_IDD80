89540485c457e60a1ca2536e7cadd54e9f7d941e
Iván Cáceres <icaceres@zytrust.com>
2025-04-30 12:51:18 -0500

    4.45.2

--- Sources/BiometricSDKAlgorithmPlugin_F6_0_IDD80.xcframework/ios-arm64_x86_64-simulator/BiometricSDKAlgorithmPlugin_F6_0_IDD80.framework/Headers/BiometricSDKAlgorithmPlugin_F6_0_IDD80.h
+++
@@ -1,6 +0,0 @@
-@interface BiometricSDKAlgorithmPlugin_F6_0_IDD80 : NSObject
-
-- (instancetype)init;
-- (void)someMethod;
-
-@end
---
+++ Sources/BiometricSDKAlgorithmPlugin_F6_0_IDD80.xcframework/ios-arm64_x86_64-simulator/BiometricSDKAlgorithmPlugin_F6_0_IDD80.framework/Headers/SimulatorStub.h
@@ -0,0 +1,18 @@
+//
+//  SimulatorStub.h
+//  SimulatorStub
+//
+//  Copyright © 2025 IDEMIA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SimulatorStub.
+FOUNDATION_EXPORT double SimulatorStubVersionNumber;
+
+//! Project version string for SimulatorStub.
+FOUNDATION_EXPORT const unsigned char SimulatorStubVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimulatorStub/PublicHeader.h>
+
+
